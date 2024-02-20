cde27ea18f382d90a92dd7cfa4efc9d2b942b79a
Karolina Reczulska <karolina.reczulska@gmail.com>
2024-02-20 15:11:11 +0100

    added js for mobile menu, added burger button animation

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('.nav-mobile');
+const navBtn = document.querySelector('.burger-btn');
+const allNavItems = document.querySelectorAll('.nav-mobile__item');
+
+const openNav = () => {
+	nav.classList.toggle('nav-mobile--active');
+    navBtn.classList.toggle('bbtn-animation-open')
+	allNavItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav-mobile--active');
+            navBtn.classList.remove('bbtn-animation-open')
+		});
+	});
+};
+
+
+navBtn.addEventListener('click', openNav);
