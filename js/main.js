b02d98e0ce0a2d9ef5992ae34877771dc6326b22
Karolina Reczulska <karolina.reczulska@gmail.com>
2024-02-29 11:38:42 +0100

    solved problem with eventListeners on menu items, changed desktop menu links animation, small changes

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav-mobile__item');
 
 const toggleNav = () => {
-	nav.classList.toggle('nav-mobile--active');
-	navBtn.classList.toggle('bbtn-animation-open');
-	allNavItems.forEach((item) => {
-		item.addEventListener('click', () => {
-			nav.classList.remove('nav-mobile--active');
-			navBtn.classList.remove('bbtn-animation-open');
-		});
-	});
+    nav.classList.toggle('nav-mobile--active');
+    navBtn.classList.toggle('bbtn-animation-open');
 };
+
+allNavItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        nav.classList.remove('nav-mobile--active');
+        navBtn.classList.remove('bbtn-animation-open');
+    });
+});
 
 navBtn.addEventListener('click', toggleNav);
